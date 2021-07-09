e7e7a580f6c6a19a485e51dbc9db52b30dc9c55a
Shripath S <shripath@madstreetden.com>
2021-07-09 23:10:11 +0530

    Absolutely final

---
+++ precache-manifest.7f2b6360b9995455af03c31d920f4234.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5593e68f093728743fb9",
+    "url": "/static/css/main.f2b7f205.chunk.css"
+  },
+  {
+    "revision": "5593e68f093728743fb9",
+    "url": "/static/js/main.5593e68f.chunk.js"
+  },
+  {
+    "revision": "3dd765f0c326d550c538",
+    "url": "/static/css/1.8a912c69.chunk.css"
+  },
+  {
+    "revision": "3dd765f0c326d550c538",
+    "url": "/static/js/1.3dd765f0.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "604f7e372631d27d560baff5899b802b",
+    "url": "/index.html"
+  }
+];
